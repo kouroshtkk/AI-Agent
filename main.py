f0cab54a47a9a13b01aeee17ba37eaa2f892e88e
kourosh sarabadani <kouroshtkk@gmail.com>
2026-07-06 17:53:20 +0200

    added verbose argument

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from dotenv import load_dotenv
 import argparse
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt",type=str,help="User prompt")
+parser.add_argument("--verbose",action="store_true",help="Enable verbose output")
 args = parser.parse_args()
+verbose=args.verbose
+
 load_dotenv()
 api_key = os.environ.get("OPENROUTER_API_KEY")
 if not api_key:
@@ -22,7 +25,9 @@ messages=[
 new_response = client.chat.completions.create(model="openai/gpt-oss-20b:free",messages=messages)
 if not new_response.usage:
     raise Exception("usage api failure")
-print(f"Prompt tokens: {new_response.usage.prompt_tokens}")
-print(f"Response tokens: {new_response.usage.completion_tokens}")
+if verbose:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {new_response.usage.prompt_tokens}")
+    print(f"Response tokens: {new_response.usage.completion_tokens}")
 print(new_response.choices[0].message.content)
 
